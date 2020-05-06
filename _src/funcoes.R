415a109ed7b2851039e4cc46fcec777d4e92a9e9
saramortara <saramortara@gmail.com>
2020-05-06 02:18:28 -0300

    adiciona funcoes que cria objetos proj.zoo de nowcasting

--- _src/funcoes.R
+++ _src/funcoes.R
@@ -198,7 +198,12 @@ Re.com.data <- function(ncasos, datas, dia0 = min(datas), delay = 5){
     return(Re)
 }
 
-#' Função para automatizar a preparação dos dados por unidade administrativa
+
+
+################################################################################
+## Funcao para proeparacao dos dados de nowcasting
+################################################################################
+#' Função para automatizar a preparação dos dados de nowcasting por unidade administrativa
 #' @details Retira datas dos sufixos dos nomes das bases e identifica a maior data. Só funciona se os nomes das bases forem mantidos no padrão
 #' @param tipo Caractere. Nome da base de dados para preparar. Tipos possíveis: `covid` para casos de COVID-19, `srag` para casos de SRAG, `obitos_covid` para óbitos por COVID-19 e `obitos_srag` para óbitos por SRAG
 #' @param adm Caractere. Unidade administrativa `estado` ou `municipio`
@@ -292,4 +297,61 @@ prepara.dados <- function(tipo = "covid",
                  now.lista = now.lista)
     
     return(pred)
+}
+
+
+
+################################################################################
+## Funcao para projecao do n acumulado de casos por data do sintoma
+################################################################################
+#' Funcao para projecao do n acumulado de casos por data do sintoma
+#' @param pred Data frame. Objeto `now.pred.zoo` gerado em prepara_nowcasting.R
+#' @param pred.original Data frame. Objeto `now.pred.original` gerado em prepara_nowcasting.R
+#' @param now.lista Lista. Objeto `now.lista` gerado em prepara_dados_nowcasting.R
+now.proj <- function(pred, 
+                     pred.original, 
+                     now.lista,
+                     n.dias = 5){
+    ## N de dias para projetar: 5 dias a partir da data atual
+    ## Adiciona ao forecast dias entre a ultima data de nocasting e o dia atual
+    days.to.forecast <- as.integer(Sys.Date() - max(time(pred)) + n.dias)
+    ## Objeto zoo com n de casos previstos pelo nowcasting concatenados com o n de casos
+    ## projetado a partir do nowcasting acumulado com regressão Poisson
+    now.proj.zoo <- merge(
+        now.mean.c = c(forecast.exponential(pred$estimate.merged.c,
+                                            start = length(time(pred)) - 4,
+                                            days.forecast = days.to.forecast)$predito,
+                       pred$estimate.merged.c),
+        
+        now.low.c = c(forecast.exponential(pred$lower.merged.c,
+                                           start = length(time(pred)) - 4,
+                                           days.forecast = days.to.forecast)$predito,
+                      pred$lower.merged.c),
+        
+        now.upp.c = c(forecast.exponential(pred$upper.merged.c,
+                                           start = length(time(pred)) - 4,
+                                           days.forecast = days.to.forecast)$predito,
+                      pred$upper.merged.c)
+    )
+    ## Adiciona vetor com n de casos notificados e os previstos para os proximos dias pela projecao
+    ## Previsto de casos notificados é calculado a partir das ditribuicoes de atrasos do nowcasting
+    ## N de dias que foram corrigidos por nowcating
+    ndias.now <- nrow(pred.original)
+    now.proj.zoo$not.mean <- c(pred$n.casos,
+                               estima.not(diff(now.proj.zoo$now.mean.c[(nrow(now.proj.zoo) - ndias.now):nrow(now.proj.zoo)]),
+                                          now.lista,
+                                          from = ndias.now - days.to.forecast + 1))
+    now.proj.zoo$not.low <- c(pred$n.casos,
+                              estima.not(diff(now.proj.zoo$now.low.c[(nrow(now.proj.zoo) - ndias.now):nrow(now.proj.zoo)]),
+                                         now.lista,
+                                         from = ndias.now - days.to.forecast + 1))
+    now.proj.zoo$not.upp <- c(pred$n.casos,
+                              estima.not(diff(now.proj.zoo$now.upp.c[(nrow(now.proj.zoo) - ndias.now):nrow(now.proj.zoo)]),
+                                         now.lista,
+                                         from = ndias.now - days.to.forecast + 1))
+    ##Calcula n de casos cumulativos
+    now.proj.zoo$not.mean.c <- cumsum(now.proj.zoo$not.mean)
+    now.proj.zoo$not.low.c <- cumsum(now.proj.zoo$not.low)
+    now.proj.zoo$not.upp.c <- cumsum(now.proj.zoo$not.upp)
+    return(now.proj.zoo)
 }
